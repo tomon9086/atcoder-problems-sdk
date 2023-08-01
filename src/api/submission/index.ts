e6d8913bb1e74db07e048c95e8bd443d37d3c3b2
tomon9086 <tomon9086@gmail.com>
2023-08-01 16:45:37 +0900

    add RetrieveUserSubmissionResponse type

--- src/api/submission/index.ts
+++ src/api/submission/index.ts
@@ -1,5 +1,20 @@
 import { EndpointName, apiUrl } from '@/util'
 
+type UserSubmission = {
+  id: number
+  epoch_second: number
+  problem_id: string
+  contest_id: string
+  user_id: string
+  language: string
+  point: number
+  length: number
+  result: 'AC' | 'WA' | 'TLE' | 'MLE' | 'RE'
+  execution_time: number
+}
+
+type RetrieveUserSubmissionResponse = UserSubmission[]
+
 export const retrieveUserSubmission = async (
   userName: string,
   fromSecond: string | number
@@ -7,6 +22,6 @@ export const retrieveUserSubmission = async (
   const res = await fetch(
     apiUrl(EndpointName.userSubmission, userName, fromSecond)
   )
-  const json = await res.json()
-  console.log(json)
+  const json = (await res.json()) as RetrieveUserSubmissionResponse
+  return json
 }
